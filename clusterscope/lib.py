25877364f7ae514ff2406998b085dc950cdc99df
luccabb <luccab@meta.com>
2025-05-23 19:02:41 +0000

    copyright headers

--- clusterscope/lib.py
+++ clusterscope/lib.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 from clusterscope.cluster_info import ClusterInfo
 
 cluster_info = ClusterInfo()
