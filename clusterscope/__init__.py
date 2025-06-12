656e929269f01560d857f8907e8e40b669c69f95
luccabb <luccab@meta.com>
2025-06-12 22:44:23 +0000

    add version back to file

--- clusterscope/__init__.py
+++ clusterscope/__init__.py
@@ -3,6 +3,8 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+__version__ = "0.0.0"
+
 from clusterscope.lib import cluster, slurm_version
 
 __all__ = ["cluster", "slurm_version"]
