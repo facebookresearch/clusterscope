261cedcb034aa3e90163cac59ad5ad56a64ed8b7
Lucca Bertoncini <32229669+luccabb@users.noreply.github.com>
2025-10-09 18:58:04 -0700

    Website - title fix (#124)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -45,8 +45,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Clusterscope is a CLI and python library to extract information from HPC Clusters.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
